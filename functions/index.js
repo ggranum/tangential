bce1a9b56a9ea9d601112dd5685e70bc59cafac7
ggranum <geoff.granum@gmail.com>
2017-05-02 10:12:19 +0100

    feat(database-functions): Add demo Firebase functions

---
+++ functions/index.js
@@ -0,0 +1,28 @@
+const functions = require('firebase-functions');
+
+
+const admin = require('firebase-admin');
+admin.initializeApp(functions.config().firebase);
+
+
+// // Create and Deploy Your First Cloud Functions
+// // https://firebase.google.com/docs/functions/write-firebase-functions
+//
+exports.helloWorld = functions.https.onRequest((request, response) => {
+  let msg = ["Hello from Firebase!"]
+  msg.push(request.acceptsLanguages())
+  response.send(msg.join("<br/>\n"));
+});
+
+
+// Take the text parameter passed to this HTTP endpoint and insert it into the
+// Realtime Database under the path /messages/:pushId/original
+exports.addMessage = functions.https.onRequest((req, res) => {
+  // Grab the text parameter.
+  const original = req.query.text;
+  // Push it into the Realtime Database then send a response
+  admin.database().ref('/messages').push({original: original}).then(snapshot => {
+    // Redirect with 303 SEE OTHER to the URL of the pushed object in the Firebase console.
+    res.redirect(303, snapshot.ref);
+  });
+});
