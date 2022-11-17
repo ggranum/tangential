a7da93f51ca74a9b0b3096a3d84aed8f944fa81c
ggranum <geoff@granum.biz>
2022-11-17 09:11:23 +0100

    Build: Gulp tasks require completion notification.

--- tools/gulp/tasks/version-bump.ts
+++ tools/gulp/tasks/version-bump.ts
@@ -1,30 +1,31 @@
 import {task} from 'gulp';
 import * as minimist from 'minimist'
 import * as path from 'path'
+
 const jsonFile = require('jsonfile');
 
 import {PROJECT_ROOT, SOURCE_ROOT} from '../constants';
-import {collectComponents} from "../task_helpers";
-import {PackageDescriptor, NpmPackageMaker} from "../package-descriptor";
+import {collectComponents} from '../task_helpers';
+import {PackageDescriptor, NpmPackageMaker} from '../package-descriptor';
 
 const argv = minimist(process.argv.slice(3));
 
 
-const versionBumpPaths = function (paths: string[], bump: any, qualifier: string|string) {
-  let modules: {[key: string]: PackageDescriptor} = {}
+const versionBumpPaths = function (paths: string[], bump: any, qualifier: string) {
+  let modules: { [key: string]: PackageDescriptor } = {}
   paths.forEach((componentPath: string) => {
     let module: PackageDescriptor = readModuleDescriptor(componentPath)
     modules[module.name] = module
   })
   let rootModule = readModuleDescriptor(PROJECT_ROOT)
 
-  let maker:NpmPackageMaker = new NpmPackageMaker(rootModule, modules, bump, qualifier)
+  let maker: NpmPackageMaker = new NpmPackageMaker(rootModule, modules, bump, qualifier)
   let fullModules = maker.updateModules()
 
-  Object.keys(fullModules).forEach((key:string)=>{
+  Object.keys(fullModules).forEach((key: string) => {
     writeModuleDescriptor(fullModules[key])
   })
-  writeModuleDescriptor(maker.updatedRootModule())
+  return writeModuleDescriptor(maker.updatedRootModule())
 };
 
 function readModuleDescriptor(componentPath: string): PackageDescriptor {
@@ -35,28 +36,33 @@ function readModuleDescriptor(componentPath: string): PackageDescriptor {
   return module
 }
 
-function writeModuleDescriptor(module: PackageDescriptor):void {
+function writeModuleDescriptor(module: PackageDescriptor): void {
   let path = module.filePath
   delete module.filePath
   jsonFile.writeFileSync(path, module, {spaces: 2})
 }
 
 
-export function versionBump() {
+// noinspection JSUnusedGlobalSymbols
+/**
+ * This task is purely synchronous. Gulp requires some form of completion notification beyond a simple return.
+ */
+export async function versionBump(): Promise<void> {
   const bump = argv['bump'];
   const beta = argv['beta'];
   const alpha = argv['alpha'];
   const qualifier = alpha ? 'alpha' : beta ? 'beta' : null
 
   if (!bump) {
-    console.log("You can specify a bump level with --bump=[<newversion> | major | minor | patch | premajor | preminor | prepatch | prerelease | from-git].");
-    console.log("Publishing using 'patch'.");
+    console.log(
+      'You can specify a bump level with --bump=[<newversion> | major | minor | patch | premajor | preminor | prepatch | prerelease | from-git].');
+    console.log('Publishing using \'patch\'.');
   } else {
     console.log(`Publishing using the ${bump} tag.`);
   }
 
   if (!beta && !alpha) {
-    console.log("You can increment a pre-release qualifier with '--bump=prerelease --[alpha|beta]'.");
+    console.log('You can increment a pre-release qualifier with \'--bump=prerelease --[alpha|beta]\'.');
   }
   let paths: string[] = collectComponents(SOURCE_ROOT)
   paths.sort()
